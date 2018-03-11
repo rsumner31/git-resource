65a726905adeb6071e401fc84d89cd5362c3f8cd
Robert Sumner Jr <rsumner31@gmail.com>
2018-03-11 04:47:07 -0500

    Revert "include requested version in check"
    This reverts commit 3d57939e0c62e19ce4775006f32a7a2240eaa0e6.
    
    Signed-off-by: Yucheng Tu <ytu@pivotal.io>
    
    Conflicts:
    	assets/check
    	test/check.sh

--- test/check.sh
+++ test/check.sh
@@ -88,7 +88,6 @@ it_can_check_from_a_ref() {
 
   check_uri_from $repo $ref1 | jq -e "
     . == [
-      {ref: $(echo $ref1 | jq -R .)},
       {ref: $(echo $ref2 | jq -R .)},
       {ref: $(echo $ref3 | jq -R .)}
     ]
@@ -184,18 +183,8 @@ it_skips_ignored_paths() {
     . == [{ref: $(echo $ref2 | jq -R .)}]
   "
 
-  check_uri_from_ignoring $repo $ref1 "file-a" | jq -e "
-    . == [
-      {ref: $(echo $ref2 | jq -R .)},
-      {ref: $(echo $ref3 | jq -R .)}
-    ]
-  "
-
   check_uri_from_ignoring $repo $ref1 "file-c" | jq -e "
-    . == [
-      {ref: $(echo $ref1 | jq -R .)},
-      {ref: $(echo $ref2 | jq -R .)}
-    ]
+    . == [{ref: $(echo $ref2 | jq -R .)}]
   "
 
   local ref4=$(make_commit_to_file $repo file-b)
@@ -206,7 +195,6 @@ it_skips_ignored_paths() {
 
   check_uri_from_ignoring $repo $ref1 "file-c" | jq -e "
     . == [
-      {ref: $(echo $ref1 | jq -R .)},
       {ref: $(echo $ref2 | jq -R .)},
       {ref: $(echo $ref4 | jq -R .)}
     ]
@@ -264,11 +252,7 @@ it_checks_given_ignored_paths() {
   "
 
   check_uri_from_paths_ignoring $repo $ref1 'file-*' 'file-b' | jq -e "
-    . == [{ref: $(echo $ref1 | jq -R .)}]
-  "
-
-  check_uri_from_paths_ignoring $repo $ref1 'file-*' 'file-a' | jq -e "
-    . == [{ref: $(echo $ref2 | jq -R .)}]
+    . == []
   "
 
   local ref4=$(make_commit_to_file $repo file-b)
@@ -289,7 +273,6 @@ it_checks_given_ignored_paths() {
 
   check_uri_from_paths_ignoring $repo $ref1 'file-*' 'file-b' | jq -e "
     . == [
-      {ref: $(echo $ref1 | jq -R .)},
       {ref: $(echo $ref5 | jq -R .)},
       {ref: $(echo $ref6 | jq -R .)}
     ]
@@ -297,7 +280,6 @@ it_checks_given_ignored_paths() {
 
   check_uri_from_paths_ignoring $repo $ref1 'file-*' 'file-b' 'file-c' | jq -e "
     . == [
-      {ref: $(echo $ref1 | jq -R .)},
       {ref: $(echo $ref5 | jq -R .)}
     ]
   "
@@ -401,7 +383,6 @@ it_can_check_empty_commits() {
 
   check_uri_from $repo $ref1 | jq -e "
     . == [
-      {ref: $(echo $ref1 | jq -R .)},
       {ref: $(echo $ref2 | jq -R .)}
     ]
   "
